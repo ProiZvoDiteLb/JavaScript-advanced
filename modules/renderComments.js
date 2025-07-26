6df9724a1133122665d848c5bc3bc5f608db8e4d
Igor Ivanov <profrontend@mail.ru>
2025-07-26 21:27:42 +0300

    разбитие JS на модули

---
+++ modules/renderComments.js
@@ -0,0 +1,33 @@
+import { comments } from "./comments.js";
+import { initLikeButtons, initCommentClick } from "./initListener.js";
+
+const commentsList = document.getElementById("comments-list");
+
+export const renderComments = () => {
+  commentsList.innerHTML = comments
+    .map((comment, index) => {
+      return `
+        <li class="comment" data-index="${index}">
+          <div class="comment-header">
+            <div>${comment.name}</div>
+            <div>${comment.date}</div>
+          </div>
+          <div class="comment-body">
+            <div class="comment-text">${comment.text}</div>
+          </div>
+          <div class="comment-footer">
+            <div class="likes">
+              <span class="likes-counter">${comment.likes}</span>
+              <button class="like-button ${
+                comment.isLiked ? "-active-like" : ""
+              }" data-index="${index}"></button>
+            </div>
+          </div>
+        </li>
+      `;
+    })
+    .join("");
+
+  initLikeButtons(); // Инициализируем обработчики кликов для кнопок лайков
+  initCommentClick(); // Инициализация обработчика клика по комментариям
+};
